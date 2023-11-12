5330a136bdf2ebb330564df6490e2ccc8c358611
Yuri Carias <yuristricken7@gmail.com>
2023-11-12 00:30:40 -0300

    atualização routes

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -18,8 +18,8 @@ export default () => {
                     <Route exact path="/config" element={<Config />} />
                     <Route exact path="/FiltrarEvento" element={<FiltrarEvento />} />
                     <Route exact path="/DescubraEvento" element={<DescubraEvento />} />
-                    <Route excat path="/Competencias" element={<Competencias />} />
-                    <Route excat path="/Nivelamento" element={<Nivelamento />} />
+                    <Route exact path="/Competencias" element={<Competencias />} />
+                    <Route exact path="/Nivelamento" element={<Nivelamento />} />
                 </Routes>
         
     );
